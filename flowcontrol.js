03f08c3f27f6759f511a46016d33896b8f76f855
calkoe <calvin.koecher@outlook.de>
2020-05-03 19:59:43 +0200

    Add files via upload

---
+++ flowcontrol.js
@@ -0,0 +1,73 @@
+const match = function (filter, topic) {
+    const filterArray = filter.split('/')
+    const length = filterArray.length
+    const topicArray = topic.split('/')
+    for (var i = 0; i < length; ++i) {
+        var left = filterArray[i]
+        var right = topicArray[i]
+        if (left === '#') return topicArray.length >= length - 1
+        if (left !== '*' && left !== right) return false
+    }
+    return length === topicArray.length
+};
+
+module.exports = function(RED) {
+
+    function flowcontrolIn(config){
+        RED.nodes.createNode(this,config);
+        var node        = this;
+        node.on('input', function(msg){
+
+            //Version
+            if(config.version){
+                if(!msg.version){msg.version = [];}
+                msg.version.push(JSON.parse(JSON.stringify(msg)));
+            }
+
+            //Conext
+            if((msg.payload||{}).Context == config.context || (msg.hap||{}).context == config.context) return;
+
+            //Send
+            if(!msg.payload || typeof msg.payload !== 'object'){msg.payload = {};}msg.payload.Context = config.context;
+            for(var t of config.topic.split(","))
+                RED.events.emit("flowcontrolLoop",{"payload":msg.payload,"topic":t,"version":msg.version,"time":Date.now()});
+       
+        });
+    }
+    RED.nodes.registerType("flowcontrolIn",flowcontrolIn);
+
+    function flowcontrolOut(config) {
+        RED.nodes.createNode(this,config);
+        var node        = this;
+        var checkTopic = function(topics,topic){
+            for(var t of topics)
+                if(match(t, topic)) return true; else continue;
+            return false
+        }
+        var handler = function(msg){
+            
+            //Topic
+            if(!checkTopic(config.topic.split(","),msg.topic)) return;
+
+            //Context
+            if((msg.payload||{}).Context == config.context || (msg.hap||{}).context == config.context) return;
+            
+            //Retained
+            if(config.retained && msg.version && msg.version[0].retain) return;
+
+            //Blacklist Topic
+            if(checkTopic(config.blTopic.split(","),msg.topic)) return;
+
+            //Blacklist Obj
+            for(var o of config.blObj.split(","))
+                if(o in msg.payload) return;
+
+            node.send(msg);
+        }
+        RED.events.on("flowcontrolLoop",handler);
+    }
+    RED.nodes.registerType("flowcontrolOut",flowcontrolOut);
+
+
+
+}
